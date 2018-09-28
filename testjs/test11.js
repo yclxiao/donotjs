5e4ae153aad5e90d1e4cfe3e9d0d37363225aea5
OF1089（杨成龙） <yangchenglong@qianmi.com>
2018-09-28 11:40:39 +0800

    test

--- testjs/test11.js
+++ testjs/test11.js
@@ -2,4 +2,38 @@ let result = [1,2,3,4,5,6].reduce((x,y) => {
     return x + y;
 });
 
-console.log(result)
+console.log(result);
+
+let a = 1;
+let b = 2;
+let c = b = a;
+console.log(a);
+console.log(b);
+console.log(c);
+
+let aaa = {
+    k: 1
+};
+
+let bbb = {
+    m: 2
+}
+
+let ccc = {
+    n: 3
+}
+console.log(aaa);
+console.log(bbb);
+console.log(ccc);
+
+let ddd = bbb = ccc;
+
+console.log(aaa);
+console.log(bbb);
+console.log(ccc);
+console.log(ddd);
+
+
+console.log(bbb === ccc);
+console.log(ddd === ccc);
+console.log(ddd === bbb);

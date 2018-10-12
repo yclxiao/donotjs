ef9263b6582d3127496859cf1c6be391f999fd06
OF1089（杨成龙） <yangchenglong@qianmi.com>
2018-10-12 11:48:50 +0800

    递归

--- donotknow-js/test18.js
+++ donotknow-js/test18.js
@@ -1,12 +1,12 @@
 //递归
-function factorial(n) {
+/* function factorial(n) {
   if (n === 1) {
     return n;
   }
   return n * factorial(n - 1);
 }
 
-console.log(factorial(5));
+console.log(factorial(5)); */
 
 
 // function factorial (n, total=1) {
@@ -16,5 +16,28 @@ console.log(factorial(5));
 //     return factorial(n-1, n * total)
 // }
 // console.log(factorial(5))
+
+
+/* var fs = (function test(num){
+  console.log(num)
+  if(num <= 1){
+      return 1;
+  }else{
+      var result = num * test(num-1);
+      return result;
+  }
+});
+console.log(fs(5)); */
+
+//递归函数，每次都会将函数的执行结果返回，函数继续执行自己
+function test(num) {
+  if (num === 1) {
+    return num;
+  }
+
+  return num * test(num -1);
+}
+
+console.log(test(5))
 
 

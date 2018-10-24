c00f114d3fd2531aef77e2ff948276b23e3259c7
OF1089（杨成龙） <yangchenglong@qianmi.com>
2018-10-24 15:25:45 +0800

    原型概念再记录

--- donotknow-js/js/test3.js
+++ donotknow-js/js/test3.js
@@ -15,9 +15,6 @@
 // Foo('111')
 // console.log(global.name)
 
-
-
-
 // function Foo(name) {
 //     this.name = name
 // }
@@ -36,7 +33,6 @@
 // var a = new Foo();
 // console.log(a.constructor === Foo);
 
-
 // function Foo() {
 //     console.log(this)
 // }
@@ -48,43 +44,57 @@
 // console.log(a.constructor === Object)
 
 
-
+/* 
 function Foo(name) {
-    this.name = name
+  this.name = name;
 }
 
-Foo.prototype.myName = function () {
-    return this.name
-}
+Foo.prototype.myName = function() {
+  return this.name;
+};
 
 function Bar(name, label) {
-    Foo.call(this, name);
-    this.label = label;
+  Foo.call(this, name);
+  this.label = label;
 }
 
-Bar.prototype = Object.create(Foo.prototype)
+Bar.prototype = Object.create(Foo.prototype);
 
-Bar.prototype.myLabel = function () {
-    return this.label
-}
+Bar.prototype.myLabel = function() {
+  return this.label;
+};
 
-var a = new Bar("a", "obj a")
+var a = new Bar("a", "obj a");
 a.myName();
 a.myLabel();
-console.log(a.__proto__.__proto__.__proto__)
+console.log(a.__proto__.__proto__.__proto__);
+ */
 
 
 
 
 
+function Animal(name) {
+  this.name = name;
+}
+Animal.prototype.getName = function() {
+  return this.name;
+};
 
+function Cat(name, age) {
+  Animal.call(this, name);
+  this.age = age || 1;
+}
+//创建对象：cat.__proto__指向Cat.prototype，
+//而Cat.prototype包含（Animal.prototype 和 constructor），
+//constructor又指向Cat函数本身，Cat构造函数就是Cat
+Cat.prototype = Object.create(Animal.prototype, { constructor: Cat });
+Cat.prototype.meow = function() {
+  return `${this.getName()}  eowww~~~~~, I'm ${this.age} year(s) old`;
+};
 
-
-
-
-
-
-
-
+var cat = new Cat('kit',11)
+console.log(cat.meow());
+console.log(cat);
 
 
